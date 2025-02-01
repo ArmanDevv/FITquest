8e3323945a7998371531bbeb9cdd57b53499508b
armandevv <armansingh7827@gmail.com>
2025-02-01 19:21:31 +0530

    Slider is Created

---
+++ src/components/home/home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./home.css"
+import { Zoom } from 'react-slideshow-image';
+import image1 from "../../assets/image1.png"
+import image2 from "../../assets/image2.png"
+import image3 from "../../assets/image3.png"
+import image4 from "../../assets/image4.png"
+const images = [image1, image2, image3, image4];
+
+const zoomOutProperties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  scale: 0.4,
+  arrows: true
+};
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Zoom {...zoomOutProperties}>
+          {images.map((each, index) => (
+            <img key={index} style={{ width: "100%" }} src={each} />
+          ))}
+        </Zoom>
+      </div>
+    );
+  };
+
+const Home = () => {
+  return (
+    <div>
+        <Slideshow />
+    </div>
+  )
+}
+
+export default Home
